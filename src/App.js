f6276ab0a59bcae05c6d740fd643d5cb44fc75e5
borjagm <borjagm08@gmail.com>
2024-11-28 17:43:48 +0100

    feature1: Añadido Toolbar del titulo de la APP

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
+import { AppBar, Toolbar, Typography } from '@mui/material';
 import React, { Suspense, lazy } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import PodcastsIcon from '@mui/icons-material/Podcasts';
 
 // Cargar los componentes de manera diferida con React.lazy
 const Home = lazy(() => import('./pages/Home/Home.jsx'));
@@ -9,6 +11,14 @@ const PodcastCapDetail = lazy(() => import('./pages/PodcastCapDetail/PodcastCapD
 function App() {
   return (
     <Router>
+      <AppBar position="static" sx={{ backgroundColor:"transparent", boxShadow:"none"}}>
+        <Toolbar>
+          <Typography variant="h5" component={Link} to="/" sx={{ textDecoration: 'none', color: '#FF4081', display: 'flex', alignItems: 'center' }}>
+            <PodcastsIcon sx={{ marginRight: '8px', color: '#FF4081' }} />
+            Podcaster
+          </Typography>
+        </Toolbar>
+      </AppBar>
       {/* Suspense permite definir un fallback mientras se cargan los componentes */}
       <Suspense fallback={<div>Loading...</div>}>
         <Routes>
