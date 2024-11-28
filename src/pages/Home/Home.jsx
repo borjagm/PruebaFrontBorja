5200a8594976b42ce83840b3e76e9211bcf8d207
borjagm <borjagm08@gmail.com>
2024-11-28 16:28:39 +0100

    feature1: Añadido MUI y ejemplo basico cards

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,6 @@
 import React, { useContext } from 'react';
 import { PodcastContext } from '@context/PodcastContext';
+import { Card, CardContent, CardMedia, Grid, Typography } from '@mui/material';
 
 const Home = () => {
   const { podcasts, loading, error } = useContext(PodcastContext);
@@ -16,8 +17,29 @@ const Home = () => {
 
   return (
     <div>
-      <h1>Probando</h1>
-      {/* Renderiza la lista de podcasts aquí */}
+      <h1>Podcasts</h1>
+      <Grid container spacing={2}>
+        {podcasts.map((podcast, index) => (
+          <Grid item xs={12} sm={6} md={4} key={index}>
+            <Card>
+              <CardMedia
+                component="img"
+                height="170"
+                image={podcast['im:image'].find(img => img.attributes.height === '170').label}
+                alt={podcast['im:name'].label}
+              />
+              <CardContent>
+                <Typography variant="h5" component="div">
+                  {podcast['im:name'].label}
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  Author: {podcast['im:artist'].label}
+                </Typography>
+              </CardContent>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
     </div>
   );
 };
