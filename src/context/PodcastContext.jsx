b382c9c9272e151bf212fbe84b3bdd99d080d4d0
borjagm <borjagm08@gmail.com>
2024-11-28 16:10:26 +0100

    feature1: Obtencion podcast 1 vez al dia
    Se controla que los podcasts se obtengan solo una vez al día.

--- src/context/PodcastContext.jsx
+++ src/context/PodcastContext.jsx
@@ -11,10 +11,28 @@ export const PodcastProvider = ({ children }) => {
   useEffect(() => {
     const fetchPodcasts = async () => {
       try {
+        const lastFetch = localStorage.getItem('lastFetch');
+        const now = new Date().getTime();
+        
+        //Controlamos que la última vez que se hizo la petición no haya sido hace más de 24 horas
+        if (lastFetch && now - lastFetch < (24*60*60*1000)) {
+          const cachedPodcasts = JSON.parse(localStorage.getItem('podcasts'));
+          if (cachedPodcasts) {
+            setPodcasts(cachedPodcasts);
+            setLoading(false);
+            return;
+          }
+        }
+
         const data = await fetchData(
           'https://itunes.apple.com/us/rss/toppodcasts/limit=100/genre=1310/json'
         );
-        setPodcasts(data.feed.entry || []);
+
+        const podcastsList = data.feed.entry || [];
+        setPodcasts(podcastsList);
+        // Guardamos los podcasts en el almacenamiento local
+        localStorage.setItem('podcasts', JSON.stringify(podcastsList));
+        localStorage.setItem('lastFetch', now);
         setLoading(false);
       } catch (err) {
         setError('Failed to load podcasts');
